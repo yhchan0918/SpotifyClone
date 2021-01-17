a1b4ea0a29dbac205a1b26259826a79c14c879b6
yhchan0918 <yhchan0918@gmail.com>
2021-01-17 15:27:25 +0800

    setup

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -7,8 +7,12 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>tab1</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
